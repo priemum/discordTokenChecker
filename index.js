1bd300fe081228632137ca3fdfae3d330a89b4b4
KiuyOR <kiuy.or@icloud.com>
2020-06-04 22:53:33 +0200

    up

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const Bot = require('./checker');
-const fs = require('fs');
+const {check} = require('./checker'),
+    {writeFileSync,readFileSync} = require('fs'),
+    {bgGreen} = require('chalk'),
+    {timeout} = require('./settings/config.json'),
+    tokens = readFileSync('tokens.txt', 'utf-8').replace(/\r/gi, '').split("\n");
 
-const config = require('./settings/config.json');
+writeFileSync('utils/invalid.txt', 'utf-8');
+writeFileSync('utils/verified.txt', 'utf-8');
+writeFileSync('utils/unverified.txt', 'utf-8');
 
-fs.writeFileSync('utils/invalid.txt', '');
-fs.writeFileSync('utils/verified.txt', '');
-fs.writeFileSync('utils/unverified.txt', '');
 
-const tokens = fs.readFileSync('tokens.txt', 'utf-8').replace(/\r/gi, '').split("\n");
+let i = 0;
 
-var i = 0;
 setInterval(() => {
     if (i >= tokens.length) {
-        console.log("Done Checking Tokens!");
+        console.log(`[${bgGreen('CHEKING TOKENS !!')}]`);
         process.exit(1);
     }
-    Bot.check(tokens[i]);
+    check(tokens[i]);
     i++;
-}, config.timeout);
+}, timeout);
